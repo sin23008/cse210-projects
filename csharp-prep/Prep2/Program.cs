a6b3499560a09824dc9ea232b070a4fdd730efe9
sin23008 <sin23008@byui.edu>
2024-09-21 20:32:00 -0600

    Prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+
+        string grade = "";
+
+        if (gradePercent >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (gradePercent >= 70)
+        {
+            Console.Write("You passed!\n");
+        }
+        else
+        {
+            Console.Write("Better luck next time!\n");
+        }
     }
 }
