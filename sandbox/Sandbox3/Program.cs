347b9b47b25d4b195bf47accd56c2e20b865bae3
sin23008 <sin23008@byui.edu>
2024-09-26 21:14:09 -0600

    sanbox3

--- sandbox/Sandbox3/Program.cs
+++ sandbox/Sandbox3/Program.cs
@@ -5,21 +5,21 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new();
+        int d3 = randomGenerator.Next(1,7);
         int d1 = randomGenerator.Next(1,7);
         int d2 = randomGenerator.Next(1,7);
-        int d3 = randomGenerator.Next(1,7);
 
-        if (d2 > d3)
+        if (d1 > d2)
         {
-            (d2, d3) = (d3, d2);
+            (d1, d2) = (d2, d1);
         }
         if (d1 > d3)
         {
             (d1, d3) = (d3, d1);
         }
-        if (d1 > d2)
+        if (d2 > d3)
         {
-            (d1, d2) = (d2, d1);
+            (d2, d3) = (d3, d2);
         }
 
         Console.WriteLine($"They are {d1}, {d2}, {d3}");
