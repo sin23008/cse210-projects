422c3169c8fe11d9d71f5d317ec2645889d9ca23
sin23008 <sin23008@byui.edu>
2024-10-11 10:04:24 -0600

    Added prompts

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -9,7 +9,14 @@
     static string SelectPrompt()
     {
         Random random = new();
-        List<string> prompts = ["Prompt1", "Prompt2", "Prompt3"];
+        List<string> prompts = ["What was the best part of your day? Why did it stand out to you?",
+                                "Did you face any challenges today? How did you handle them, and what could you have done differently?",
+                                "How did you feel throughout the day? Did your mood change at any point? What influenced it?",
+                                "What is one thing you accomplished today, big or small, that made you feel proud?",
+                                "Who did you interact with today, and how did they impact your day—either positively or negatively",
+                                "What is something you learned today, whether from an experience, a person, or even a random thought?",
+                                "What is one thing from today that you are grateful for? How did it make your day better?"];
+                                
         int promptIndex = random.Next(prompts.Count);
         string prompt = prompts[promptIndex];
         return prompt;
