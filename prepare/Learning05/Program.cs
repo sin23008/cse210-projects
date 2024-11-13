e1e28c2bff26b1f3687852994fe27405e2725812
sin23008 <sin23008@byui.edu>
2024-11-13 13:42:14 -0700

    Shapes program done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [
+            new Rectangle("blue", 3, 2),
+            new Square("red", 5),
+            new Circle("orange", 2)
+        ];
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
